e8d987f54b6bf06ce72215cc3218a9ca7ffa0577
zaidejaz <zaidejaz521@gmail.com>
2024-05-28 10:27:13 +0500

    added search functionality

--- main.py
+++ main.py
@@ -25,18 +25,18 @@ def get_soup(url):
         logger.error(Fore.RED + f"Error fetching {url}: {e}")
         return None
 
-def is_valid_category(category_url, category):
-    """Check if the category URL is valid by checking if the page contains products."""
-    logger.info(f"Checking if {category} URL is valid: {category_url}")
-    soup = get_soup(category_url)
+def is_valid_link(url):
+    """Check if the URL is valid by checking if the page contains products."""
+    logger.info(f"Checking if the URL is valid: {url}")
+    soup = get_soup(url)
     if soup is None:
         return False
     products = soup.find_all("div", class_="product--box box--minimal")
     is_valid = len(products) > 0
     if is_valid:
-        logger.info(Fore.GREEN + "Valid category URL")
+        logger.info(Fore.GREEN + "Valid URL")
     else:
-        logger.warning(Fore.YELLOW + "Invalid category URL")
+        logger.warning(Fore.YELLOW + "Invalid URL")
     return is_valid
 
 def get_total_pages(soup):
@@ -88,6 +88,49 @@ def scrape_category(category_url):
     
     return all_products
 
+def search_products(search_query):
+    """Search for products using the search query."""
+    search_url = f"{BASE_URL}/en/search?p=1&followSearch=9998&q={search_query}&o=7&n=120"
+    page = 1
+    all_links = []
+    all_products = []
+    if is_valid_link(search_url):
+        logger.info(Fore.GREEN + "Valid search query. Scraping products...")
+        # Fetch the first page to get the total number of pages
+        soup = get_soup(search_url)
+        if soup is None:
+            return all_products
+
+        total_pages = get_total_pages(soup)
+        logger.info(f"Total pages found: {total_pages}")
+
+        while page <= total_pages:
+            logger.info(f"Scraping page {page} of {total_pages}")
+            url = f"{BASE_URL}/en/search?p={page}&followSearch=9998&q={search_query}&o=7&n=120"
+            soup = get_soup(url)
+            if soup is None:
+                break
+
+            # Find product containers
+            product_containers = soup.find_all("div", class_="product--box box--minimal")
+            if not product_containers:
+                break
+
+            for product in product_containers:
+                product_link = product.find("a", class_="product--title")["href"]
+                all_links.append(product_link)
+            page += 1
+
+        # Initialize tqdm for the loading indicator
+        progress_bar = tqdm(all_links, desc="Scraping Products", unit="product")
+
+        for link in progress_bar:
+            product_data = scrape_product(link)
+            if product_data:
+                all_products.append(product_data)
+
+        return all_products
+
 def scrape_product(product_url):
     """Scrape the details of a single product."""
     soup = get_soup(product_url)
@@ -133,23 +176,33 @@ def save_to_csv(products, filename):
     logger.info(Fore.GREEN + f"Saved {len(products)} products to {filename}")
 
 def main():
-    category_url = input("Enter the category URL: ")
-    # Remove the protocol and "www." from the URL
-    url_without_protocol = category_url.replace("https://", "").replace("www.", "")
+    while True:
+        print('\n' + Fore.CYAN + "Welcome to the Scooter Center Scraper!" + Style.RESET_ALL)
+        print("Choose option:")
+        print("1. Enter a category URL")
+        print("2. Search")
+        option = input("Enter your choice: ")
+        if option == "1":
+            category_url = input("Enter the category URL: ")
+            # Remove the protocol and "www." from the URL
+            url_without_protocol = category_url.replace("https://", "").replace("www.", "")
+            # Split the URL by "/"
+            url_parts = url_without_protocol.split("/")
+            # Get the category, which is the third part
+            category = url_parts[2]
 
-    # Split the URL by "/"
-    url_parts = url_without_protocol.split("/")
-
-    # Get the category, which is the third part
-    category = url_parts[2]
-
-    if is_valid_category(category_url, category):
-        logger.info(Fore.GREEN + "Valid category. Scraping products...")
-
-        products = scrape_category(category_url)
-        save_to_csv(products, f"{category}.csv")
-    else:
-        logger.error(Fore.RED + "Invalid category URL. Please try again.")
+            if is_valid_link(category_url):
+                logger.info(Fore.GREEN + "Valid category. Scraping products...")
+                products = scrape_category(category_url)
+                if products:
+                    save_to_csv(products, f"{category}.csv")
+        elif option == "2":
+            search_query = input("Enter the search query: ")
+            products = search_products(search_query)
+            if products:
+                save_to_csv(products, f"{search_query}.csv")
+        else:
+            logger.error(Fore.RED + "Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
---
+++ readme.txt
@@ -0,0 +1,20 @@
+As i am using the linux so i am sending you this file on how to run the code. Because in linux everything is different. 
+
+First of all install python 3.10 or higher
+https://python.org/downloads
+
+Then in you command prompt run this command (Note: You must be inside the scraper folder.)
+pip install poetry
+
+After it run this command
+poetry install
+
+and then
+poetry shell
+
+After successfully running this command all of you dependencies are downloaded.
+
+Then you can run the scraper
+python main.py
+
+This will start the scraper. You must not stop the scraper until its complete otherwise you have to start over.
